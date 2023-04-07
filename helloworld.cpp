002f7631272719683d79ecde2c6ad30ae8cf7006
Mr-Chenxii <1019766824@qq.com>
2023-04-07 11:42:40 +0800

    test

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: Chenxi
+ * @Date: 2023-04-07 11:42:09
+ * @LastEditors: Chenxi
+ * @LastEditTime: 2023-04-07 11:42:32
+ * @FilePath: /code1/helloworld.cpp
+ * @Description: Code Learning
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
